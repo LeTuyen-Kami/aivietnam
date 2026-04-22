88907ecebe611e8c102a9d79bdd96a1f664212db
Letuyen <letuyenhk13cqt@gmail.com>
2026-04-22 14:58:39 +0700

    fix bug

--- src/app/(payload)/admin/importMap.js
+++ src/app/(payload)/admin/importMap.js
@@ -47,6 +47,7 @@ import { CategoryColumnRowLabel as CategoryColumnRowLabel_db905e1343ea760951804a
 import { ContactLinkRowLabel as ContactLinkRowLabel_2fa3976eca5fca9ef90e9a77ed089197 } from '@/Footer/ContactLinkRowLabel'
 import { default as default_1a7510af427896d367a49dbf838d2de6 } from '@/components/BeforeDashboard'
 import { default as default_8a7ab0eb7ab5c511aba12e68480bfe5e } from '@/components/BeforeLogin'
+import { VercelBlobClientUploadHandler as VercelBlobClientUploadHandler_16c82c5e25f430251a3e3ba57219ff4e } from '@payloadcms/storage-vercel-blob/client'
 import { CollectionCards as CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1 } from '@payloadcms/next/rsc'
 
 export const importMap = {
@@ -99,5 +100,6 @@ export const importMap = {
   "@/Footer/ContactLinkRowLabel#ContactLinkRowLabel": ContactLinkRowLabel_2fa3976eca5fca9ef90e9a77ed089197,
   "@/components/BeforeDashboard#default": default_1a7510af427896d367a49dbf838d2de6,
   "@/components/BeforeLogin#default": default_8a7ab0eb7ab5c511aba12e68480bfe5e,
+  "@payloadcms/storage-vercel-blob/client#VercelBlobClientUploadHandler": VercelBlobClientUploadHandler_16c82c5e25f430251a3e3ba57219ff4e,
   "@payloadcms/next/rsc#CollectionCards": CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1
 }
